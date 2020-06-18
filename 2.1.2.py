4f021d25b694c7aff2558da05c2ccf1545da5137
OBA <ooba@activezero.co.jp>
2020-06-18 13:57:32 +0900

    New

---
+++ 2.1.2.py
@@ -0,0 +1,16 @@
+"""
+Args:
+    各変数名の一覧を記載
+    なし
+Returns:
+    型と戻り値のセマンティクス
+    なし
+Raises:
+
+"""
+the_world_is_flat = True
+if the_world_is_flat:
+    print("落ちないように気をつけて!")
+else:
+    print("飛べ！")
+
