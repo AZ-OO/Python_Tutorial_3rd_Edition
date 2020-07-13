717410f15005d67237860d9c177399d32adb0848
OBA <ooba@activezero.co.jp>
2020-07-13 13:06:41 +0900

    Update

--- 4.7.7.py
+++ 4.7.7.py
@@ -1,3 +1,11 @@
 """
 4.7.7 関数注釈(関数アノテーション)
 """
+#
+
+def my_func(ham: str, eggs: str = 'eggs2') -> str:
+    print('Annotations:', my_func.__annotations__)
+    print('Arguments:', ham, eggs)
+    return ham + ' and ' + eggs
+
+print(my_func('spam'))
