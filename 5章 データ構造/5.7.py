2925b8cc1ba8ec059c45b10c02f850d09462ee9c
OBA <ooba@activezero.co.jp>
2020-07-16 15:39:59 +0900

    Update

--- 5章 データ構造/5.7.py
+++ 5章 データ構造/5.7.py
@@ -4,7 +4,7 @@
 
 # 比較演算子in及びnot inはシーケンスに値が存在するか(および存在しないか)をチェックする
 # 演算子is及びis notは、2つのオブジェクトを比較して完全に同一であるかを調べる
-#
+
 
 srting1, string2, string3 = '','Trondheim','Hammer Dance'
 non_null = string2 or string2 or string3
