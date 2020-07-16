a1e3cec8c7a4802901f2bb7ab724635afd473c14
OBA <ooba@activezero.co.jp>
2020-07-16 15:55:39 +0900

    Update

--- 5章 データ構造/5.8.py
+++ 5章 データ構造/5.8.py
@@ -2,9 +2,10 @@
 5.8 シーケンスの比較、その他の型の比較
 """
 
-print((1, 2, 3) < (1, 2, 3))             # False
-print([1, 2, 3] < [1, 2, 3])             # False
-print('ABC' < 'C' < 'Python' < 'Python') # False
-print((1, 2, 3, 4) < (1, 2, 4))          # True
-print((1, 2) < (1, 2, -1))               # True
-print((1, 2, 3) == (1.0, 2.0, 3.0))      # True
+print((1, 2, 3) < (1, 2, 3))                         # False
+print([1, 2, 3] < [1, 2, 3])                         # False
+print('ABC' < 'C' < 'Python' < 'Python')             # False
+print((1, 2, 3, 4) < (1, 2, 4))                      # True
+print((1, 2) < (1, 2, -1))                           # True
+print((1, 2, 3) == (1.0, 2.0, 3.0))                  # True
+print((1, 2, ('aa', 'bb')) < (1, 2, ('abc', 'a'),4)) # True
