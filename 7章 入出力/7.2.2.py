edfa900ba7af30ebe78baace6f33bff44dc66fa7
OBA <ooba@activezero.co.jp>
2020-08-06 09:32:41 +0900

    Update

--- 7章 入出力/7.2.2.py
+++ 7章 入出力/7.2.2.py
@@ -2,3 +2,13 @@
 構造のあるデータをjsonで保存する
 """
 
+# PythonではJSON(javaScript Object Notaition)という
+# ポピュラーなデータ変換フォーマットが使える。
+# 標準モジュールjsonはPythonのデータ階層構造を取って文字列表現をコンバートすることができる。
+# このプロセスを『シリアライズ』という。
+# 文字列表現からデータを再構築することは『デシリアライズ』という。
+
+import json
+x = [1, 'simple', 'list']
+json.dumps(x)
+print(x)
