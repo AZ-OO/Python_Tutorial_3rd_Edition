65bf0adce7c8411e04c23d5bcfef825c4fdb9134
OBA <ooba@activezero.co.jp>
2020-08-20 15:28:01 +0900

    Edit

--- 9章 クラス/9.3.5.py
+++ 9章 クラス/9.3.5.py
@@ -13,3 +13,35 @@ print(d.kind) # すべてのいうに共通
 print(e.kind) # すべてのいうに共通
 print(d.name) # dに固有
 print(e.name) # eに固有
+
+# すべてのDogインスタンスでただ1つのリストが共有されてしまう
+class Dog2:
+
+    tricks = [] # クラス変数の使い方を間違えている
+
+    def __init__(self, name):
+        self.name = name
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
+d2 = Dog2('Fido2')
+e2 = Dog2('Buddy2')
+d2.add_trick('ころがる')
+e2.add_trick('死んだふり')
+print(d2.tricks) # すべての犬が共有しているなんて！
+
+class Dog3:
+
+    def __init__(self, name):
+        self.name = name
+        self.tricks = [] # それぞれの犬に新たに空リストを生成
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
+
+d3 = Dog3('Fido3')
+e3 = Dog3('Buddy3')
+d3.add_trick('ころがる')
+e3.add_trick('死んだふり')
+print(d3.tricks)
+print(e3.tricks)
